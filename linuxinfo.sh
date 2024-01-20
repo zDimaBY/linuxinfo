eb713fe9e7e5e0971496461b82dd5d0a890e95e5
zDimaBY <zdimaby@gmail.com>
2024-01-20 15:46:49 +0200

    v0.3

--- linuxinfo.sh
+++ linuxinfo.sh
@@ -30,61 +30,52 @@ if [ -e "/etc/os-release" ]; then
     source "/etc/os-release"
 else
     print_message "${RED}" "Error: /etc/os-release not found"
+    print_message "${RED}" "lsb_release is currently not installed, please install it:"
+
+    case "$(uname -s)" in
+        Linux)
+            if [ -x "$(command -v apt-get)" ]; then
+                print_message "${RED}" "On ${YELLOW}Debian or Ubuntu${RED}:"
+                print_message "${RED}" "sudo apt-get update && sudo apt-get install lsb-release"
+            elif [ -x "$(command -v yum)" ]; then
+                print_message "${RED}" "On ${YELLOW}Red Hat-based${RED} systems:"
+                print_message "${RED}" "sudo yum install redhat-lsb-core"
+            elif [ -x "$(command -v dnf)" ]; then
+                print_message "${RED}" "On ${YELLOW}AlmaLinux${RED}:"
+                print_message "${RED}" "sudo dnf install redhat-lsb-core"
+            elif [ -x "$(command -v zypper)" ]; then
+                print_message "${RED}" "On ${YELLOW}SUSE${RED}:"
+                print_message "${RED}" "sudo zypper install lsb-release"
+            elif [ -x "$(command -v pacman)" ]; then
+                print_message "${RED}" "On ${YELLOW}Arch Linux${RED}:"
+                print_message "${RED}" "sudo pacman -S lsb-release"
+            else
+                print_message "${RED}" "Unsupported package manager. Please install lsb-release manually."
+            fi
+            ;;
+        *)
+            print_message "${RED}" "Unsupported operating system. Please install lsb-release manually."
+            ;;
+    esac
+
     exit 1
 fi
 
-# Detect OS
-if [ -e "/etc/os-release" ] && [ ! -e "/etc/redhat-release" ]; then
-    type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '=')
-    if [ "$type" = "ubuntu" ]; then
-        # Check if lsb_release is installed
-        if [ -e '/usr/bin/lsb_release' ]; then
-            release="$(lsb_release -s -r)"
-            VERSION='ubuntu'
-        else
-            print_message "${RED}" "lsb_release is currently not installed, please install it:"
-            print_message "${RED}" "apt-get update && apt-get install lsb-release"
-            exit 1
-        fi
-    elif [ "$type" = "debian" ]; then
-        release=$(cat /etc/debian_version | grep -o "[0-9]\{1,2\}" | head -n1)
-        VERSION='debian'
-    fi
-elif [ -e "/etc/os-release" ] && [ -e "/etc/redhat-release" ]; then
-    type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '"')
-    if [ "$type" = "rhel" ]; then
-        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
-        VERSION='rhel'
-    elif [ "$type" = "almalinux" ]; then
-        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
-        VERSION='almalinux'
-    elif [ "$type" = "eurolinux" ]; then
-        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
-        VERSION='eurolinux'
-    elif [ "$type" = "rocky" ]; then
-        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
-        VERSION='rockylinux'
-    fi
-else
-    type="NoSupport"
-fi
-
-no_support_message() {
-    print_message "${RED}" "****************************************************"
-    print_message "${RED}" "Your operating system (OS) is not supported by"
-    print_message "${RED}" "Linux info. Officially supported releases:"
-    print_message "${RED}" "****************************************************"
-    print_message "${RED}" "  Debian 10, 11"
-    print_message "${RED}" "  Ubuntu 18.04 20.04, 22.04"
-    print_message "${RED}" "  AlmaLinux, EuroLinux, Red Hat EnterPrise Linux, Rocky Linux 8,9"
-    print_message "${RED}" ""
-    exit 1
-}
-
-if [ "$type" = "NoSupport" ]; then
-    no_support_message
-fi
+# Check for supported OS
+case "${ID}" in
+    "debian" | "ubuntu")
+        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN} Running on a Debian or Ubuntu system."
+        ;;
+    "rhel" | "almalinux" | "eurolinux" | "rocky" | "centos")
+        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN} Running on a Red Hat-based system."
+        ;;
+    *)
+        print_message "${RED}" "Your operating system is not officially supported."
+        print_message "${RED}" "Supported releases include: Debian 10, 11, Ubuntu 18.04, 20.04, 22.04"
+        exit 1
+        ;;
+esac
 
 # Displaying colored message before running the command
-print_message "${GREEN}" "Your operating system ${YELLOW}${type} ${VERSION_ID}${GREEN}. Running command..."
-ss -utplns
+print_message "${GREEN}" "Running 'ss -utplns' command..."
+ss -utplns 
