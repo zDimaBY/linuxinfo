dfe9d14b20433696bcf492caefa305b277631c1b
zDimaBY <zdimaby@gmail.com>
2024-01-20 19:11:42 +0200

    v0.4

--- linuxinfo.sh
+++ linuxinfo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# [ ! -f ~/.vimrc ] && { echo -e "set number\nsyntax on" > ~/.vimrc && trap 'rm ~/.vimrc' EXIT && (command -v curl &> /dev/null && curl -sSL https://raw.githubusercontent.com/zDimaBY/linuxinfo/main/linuxinfo.sh | bash || (command -v wget &> /dev/null && wget -qO- https://raw.githubusercontent.com/zDimaBY/linuxinfo/main/linuxinfo.sh -O - | bash || { print_message "${RED}" "Error: Neither 'curl' nor 'wget' found. Please install one of them to continue." && exit 1; })) && echo "Settings applied for the current session."; } || echo "File .vimrc already exists, no changes made."
+
 # Color codes
 BLACK="\e[30m"
 RED="\e[31m"
@@ -64,18 +67,23 @@ fi
 # Check for supported OS
 case "${ID}" in
     "debian" | "ubuntu")
-        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN} Running on a Debian or Ubuntu system."
+        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN}"
         ;;
     "rhel" | "almalinux" | "eurolinux" | "rocky" | "centos")
-        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN} Running on a Red Hat-based system."
+        print_message "${GREEN}" "Detected Operating System: ${YELLOW}${ID} ${VERSION_ID}${GREEN} Red Hat-based system."
         ;;
     *)
         print_message "${RED}" "Your operating system is not officially supported."
-        print_message "${RED}" "Supported releases include: Debian 10, 11, Ubuntu 18.04, 20.04, 22.04"
+        print_message "${RED}" "Supported releases include: Debian 10, 11, Ubuntu 18.04, 20.04, 22.04 and on a Red Hat-based system."
         exit 1
         ;;
 esac
 
 # Displaying colored message before running the command
-print_message "${GREEN}" "Running 'ss -utplns' command..."
-ss -utplns 
+print_message "${GREEN}" "Running command..."
+ports=(21 22 25 443 1500 3306 8083)
+for port in "${ports[@]}"; do
+    count=$(ss -an | grep ":$port " | wc -l)
+    echo "Port $port: $count"
+done
+ss -utplns
